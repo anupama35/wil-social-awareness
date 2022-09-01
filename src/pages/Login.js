5162f7b2c45708e5d2f8e748c28a2f603f543bb3
anupama35 <68852363+anupama35@users.noreply.github.com>
2022-09-01 17:34:16 +1000

    session tracking

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -24,7 +24,7 @@ function Login() {
             } else {                      
                 alert("Successfully logged in");
                 navigate("/");
-                window.location.reload(false);
+                //window.location.reload(false);
             }
 
          });
